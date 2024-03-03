98a1913f736ba58a8471fe8abf89dc188f299994
Sumit Kushwah <sumitkushwah1729@gmail.com>
2024-03-03 22:02:00 +0530

    init

---
+++ app/go.mod
@@ -0,0 +1,5 @@
+module github.com/amit-rajawat/go-app
+
+go 1.22.0
+
+require github.com/amit-rajawat/my-library v1.0.0 // indirect
---
+++ app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	library "github.com/amit-rajawat/my-library"
+)
+
+func main() {
+	fmt.Println(library.Info())
+}
